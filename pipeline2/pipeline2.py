848f3dd45511de6eb314018095743f9a74be292f
unknown <achippa2112@gmail.com>
2021-06-12 19:55:21 -0400

    xx

--- pipeline2/pipeline2.py
+++ pipeline2/pipeline2.py
@@ -4,71 +4,57 @@ import os
 import datetime, time
 import base64
 
-#Function to create DB connection
-def create_connection():
-    """ create a database connection
+#Function to run the sql queries..
+def runSqlQuery(query, params = None):
+    
     """
-    conn = None
-    try:
-      
-      # Process SSL certificates
-      if not os.path.exists('.ssl'):
-          os.makedirs('.ssl')
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    
+    if not os.path.exists('.ssl'):
+        os.makedirs('.ssl')
          
-      #Get GCS credentials 
-      filecontents = os.environ.get('PG_SSLROOTCERT').replace("@","=")
-      decoded_creds = base64.b64decode(filecontents)
-      with open('.ssl/server-ca.pem', 'w') as f1:
-          f1.write(str(decoded_creds))
+    #Get GCS credentials 
+    filecontents = os.environ.get('PG_SSLROOTCERT').replace("@","=")
+    decoded_creds = base64.b64decode(filecontents)
+    with open('.ssl/server-ca.pem', 'w') as f1:
+        f1.write(str(decoded_creds))
       
-      filecontents = os.environ.get('PG_SSLCERT').replace("@","=")
-      decoded_creds = base64.b64decode(filecontents)
-      with open('.ssl/client-cert.pem', 'w') as f1:
-          f1.write(str(decoded_creds))
+    filecontents = os.environ.get('PG_SSLCERT').replace("@","=")
+    decoded_creds = base64.b64decode(filecontents)
+    with open('.ssl/client-cert.pem', 'w') as f1:
+        f1.write(str(decoded_creds))
           
-      filecontents = os.environ.get('PG_SSLKEY').replace("@","=")
-      decoded_creds = base64.b64decode(filecontents)
-      with open('.ssl/client-key.pem', 'w') as f1:
-          f1.write(str(decoded_creds))
+    filecontents = os.environ.get('PG_SSLKEY').replace("@","=")
+    decoded_creds = base64.b64decode(filecontents)
+    with open('.ssl/client-key.pem', 'w') as f1:
+        f1.write(str(decoded_creds))
     
 
-      #Change unix permissions to restricted
-      os.chmod(".ssl/server-ca.pem",0o600)
-      os.chmod(".ssl/client-cert.pem",0o600)
-      os.chmod(".ssl/client-key.pem",0o600)
+    #Change unix permissions to restricted
+    os.chmod(".ssl/server-ca.pem",0o600)
+    os.chmod(".ssl/client-cert.pem",0o600)
+    os.chmod(".ssl/client-key.pem",0o600)
 
-      sslmode="sslmode=verify-ca"
-      sslrootcert = "sslrootcert=.ssl/server-ca.pem"
-      sslcert = "sslcert=.ssl/client-cert.pem"
-      sslkey = "sslkey=.ssl/client-key.pem"
-      hostaddr = "hostaddr={}".format(os.environ.get('PG_HOST'))
-      user = "user=postgres"
-      password = "password={}".format(os.environ.get('PG_PASSWORD'))
-      dbname="dbname=mgmt590"
+    sslmode="sslmode=verify-ca"
+    sslrootcert = "sslrootcert=.ssl/server-ca.pem"
+    sslcert = "sslcert=.ssl/client-cert.pem"
+    sslkey = "sslkey=.ssl/client-key.pem"
+    hostaddr = "hostaddr={}".format(os.environ.get('PG_HOST'))
+    user = "user=postgres"
+    password = "password={}".format(os.environ.get('PG_PASSWORD'))
+    dbname="dbname=mgmt590"
 
-      dbconnect = " ".join([sslmode,
+    dbconnect = " ".join([sslmode,
                             sslrootcert,
                             sslcert,
                             sslkey,
                             hostaddr,
                             user,
                             dbname,password])
-      conn = psycopg2.connect(dbconnect)
-    except:
-        print("Error occured")
-
-    return conn
-
-
-
-#Function to run the sql queries..
-def runSqlQuery(query, params = None):
-    """
-    Query all rows in the tasks table
-    :param conn: the Connection object
-    :return:
-    """
-    conn = create_connection() 
+    conn = psycopg2.connect(dbconnect)
     cur = conn.cursor()
     if params == None:
         cur.execute(query)
