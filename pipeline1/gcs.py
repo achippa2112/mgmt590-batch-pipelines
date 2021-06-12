a864e46c85a9a0a8b73b96863872d5bded9a4b60
unknown <achippa2112@gmail.com>
2021-06-12 05:21:16 -0400

    bugfixes

--- pipeline1/gcs.py
+++ pipeline1/gcs.py
@@ -1,5 +1,6 @@
 from google.cloud import storage
 import os
+import python_pachyderm
 
 # creating a GCS client
 client = storage.Client()
@@ -8,4 +9,5 @@ bucket = client.get_bucket('gcsrestapi')
 
 for filecsv in client.list_blobs('gcsrestapi'):
     blob = bucket.blob(filecsv.name)
-    blob.download_to_filename(os.path.join("/cron/images",filecsv.name))
+    blob.download_to_filename(os.path.join("/pfs/images",filecsv.name))
+    
