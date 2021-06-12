3d224ae1a24418d433f6710f401adb4f761605a9
unknown <achippa2112@gmail.com>
2021-06-12 03:18:22 -0400

    intial commit

--- pipeline1/gcs.py
+++ pipeline1/gcs.py
@@ -1,16 +1,11 @@
 from google.cloud import storage
+import os
 
 # creating a GCS client
 client = storage.Client()
 
-# https://console.cloud.google.com/storage/browser/[bucket-id]/
-bucket = client.get_bucket('mgmt590-class')
+bucket = client.get_bucket('gcsrestapi')
 
-print('fixed')
-
-# Push our file to the bucket
-blob = bucket.blob('test.txt')
-
-# Pull our file from the bucket
-print(blob.download_as_string())
-
+for filecsv in client.list_blobs('gcsrestapi'):
+    blob = bucket.blob(filecsv.name)
+    blob.download_to_filename(os.path.join("/pfs/out",filecsv.name))
